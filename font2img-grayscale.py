109bf4b4be5dbd0888fbacae604b6b8f34ff1587
jhyeo <jhyeo@altair.snu.ac.kr>
2017-08-24 15:08:03 +0900

    New option to create merged image for handwriting
        Use --handwriting_folder to specify handwriting image folder
        Change default option to create 128px image

--- font2img-grayscale.py
+++ font2img-grayscale.py
@@ -9,7 +9,6 @@ import os
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import json
 import collections
 
 reload(sys)
@@ -19,9 +18,9 @@ KR_CHARSET = None
 
 def get_offset(ch, font, canvas_size):
     font_size = font.getsize(ch)
-    font_offset = font.getoffset(ch);
-    offset_x = canvas_size/2 - font_size[0]/2 - font_offset[0]/2;
-    offset_y = canvas_size/2 - font_size[1]/2 - font_offset[1]/2;
+    font_offset = font.getoffset(ch)
+    offset_x = canvas_size/2 - font_size[0]/2 - font_offset[0]/2
+    offset_y = canvas_size/2 - font_size[1]/2 - font_offset[1]/2
     return [ offset_x, offset_y ]
 
 
@@ -82,12 +81,23 @@ def select_sample(charset):
     samples = []
     for i in range(399):
         samples.append(charset[28*i+(i%28)])
-        samples.append(charset[28*i+(i%28)+14])
     np.random.shuffle(samples)
     return samples
 
+
+def draw_handwriting(ch, src_font, canvas_size, src_offset, dst_folder):
+    s = ch.decode('utf-8').encode('raw_unicode_escape').replace("\\u","").upper()
+    dst_path = dst_folder + "/uni" + s + ".png"
+    dst_img = Image.open(dst_path)
+    # check the filter example in the hashes or not
+    src_img = draw_single_char(ch, src_font, canvas_size, src_offset[0], src_offset[1])
+    example_img = Image.new("RGB", (canvas_size * 2, canvas_size), (255, 255, 255)).convert('L')
+    example_img.paste(dst_img, (0, 0))
+    example_img.paste(src_img, (canvas_size, 0))
+    return example_img
+
 def font2img(src, dst, charset, char_size, canvas_size,
-             x_offset, y_offset, sample_count, sample_dir, label=0, filter_by_hash=True, fixed_sample=False, all_sample=False):
+             x_offset, y_offset, sample_count, sample_dir, label=0, filter_by_hash=True, fixed_sample=False, all_sample=False, handwriting_folder=False):
     src_font = ImageFont.truetype(src, size=char_size)
     dst_font = ImageFont.truetype(dst, size=char_size)
 
@@ -102,6 +112,17 @@ def font2img(src, dst, charset, char_size, canvas_size,
         print("filter hashes -> %s" % (",".join([str(h) for h in filter_hashes])))
 
     count = 0
+
+    if handwriting_folder:
+        train_set = select_sample(charset)
+        for c in train_set:
+            e = draw_handwriting(c, src_font, canvas_size, [x_offset, y_offset], handwriting_folder)
+            if e:
+                e.save(os.path.join(sample_dir, "%d_%04d_train.png" % (label, count)))
+                count += 1
+                if count % 100 == 0:
+                    print("processed %d chars" % count)
+        return
 
     if fixed_sample:
         train_set = select_sample(charset)
@@ -154,15 +175,16 @@ parser.add_argument('--src_font', dest='src_font', required=True, help='path of 
 parser.add_argument('--dst_font', dest='dst_font', required=True, help='path of the target font')
 parser.add_argument('--filter', dest='filter', type=int, default=0, help='filter recurring characters')
 parser.add_argument('--shuffle', dest='shuffle', type=int, default=0, help='shuffle a charset before processings')
-parser.add_argument('--char_size', dest='char_size', type=int, default=150, help='character size')
-parser.add_argument('--canvas_size', dest='canvas_size', type=int, default=256, help='canvas size')
-parser.add_argument('--x_offset', dest='x_offset', type=int, default=20, help='x offset')
-parser.add_argument('--y_offset', dest='y_offset', type=int, default=20, help='y_offset')
+parser.add_argument('--char_size', dest='char_size', type=int, default=80, help='character size')
+parser.add_argument('--canvas_size', dest='canvas_size', type=int, default=128, help='canvas size')
+parser.add_argument('--x_offset', dest='x_offset', type=int, default=27, help='x offset')
+parser.add_argument('--y_offset', dest='y_offset', type=int, default=16, help='y_offset')
 parser.add_argument('--sample_count', dest='sample_count', type=int, default=1000, help='number of characters to draw')
 parser.add_argument('--sample_dir', dest='sample_dir', help='directory to save examples')
 parser.add_argument('--label', dest='label', type=int, default=0, help='label as the prefix of examples')
 parser.add_argument('--fixed_sample', dest='fixed_sample', type=int, default=0, help='pick fixed samples (399 training set, 500 test set). Note that this should not be used with --suffle.')
 parser.add_argument('--all_sample', dest='all_sample', type=int, default=0, help='pick all possible samples (except for missing characters)')
+parser.add_argument('--handwriting_folder', dest='handwriting_folder', default=0, help='pick handwriting samples (399 training set). Note that this should not be used with --suffle.')
 
 args = parser.parse_args()
 
@@ -174,4 +196,4 @@ if __name__ == "__main__":
         np.random.shuffle(charset)
     font2img(args.src_font, args.dst_font, charset, args.char_size,
              args.canvas_size, args.x_offset, args.y_offset,
-             args.sample_count, args.sample_dir, args.label, args.filter, args.fixed_sample, args.all_sample)
+             args.sample_count, args.sample_dir, args.label, args.filter, args.fixed_sample, args.all_sample, args.handwriting_folder)

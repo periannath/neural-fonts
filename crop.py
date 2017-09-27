b0a809262e73009cea7f968cf3465a6d47865451
jhyeo <jhyeo@altair.snu.ac.kr>
2017-09-27 19:18:59 +0900

    Fix centering issue at crop.py
        Now correctly crop scanned images
        Increase constrast to get clear image

--- crop.py
+++ crop.py
@@ -2,55 +2,9 @@
 import os
 import argparse
 from PIL import Image
+from PIL import ImageOps
 from PIL import ImageFilter
-
-def crop_image_frequency(src_dir, dst_dir):
-    f = open("399-frequency.txt", "r")
-    if not os.path.exists(dst_dir):
-        os.makedirs(dst_dir)
-    for page in range(1,4):
-        img = Image.open( src_dir + "/" + str(page) +"-frequency.png").convert('L')
-        img = img.point(lambda x : 0 if x < 192 else x, 'L')
-        img = img.point(lambda x : 255 if x > 192 else x, 'L')
-        img = img.filter(ImageFilter.MedianFilter)
-        width, height = img.size
-        cell_width = width/cols
-        cell_height = height/rows
-        header_offset = height/rows * header_ratio
-        width_margin = cell_width * 0.10
-        height_margin = cell_height * 0.10
-
-        for j in range(0,rows):
-            for i in range(0,cols):
-                left = i * cell_width
-                upper = j * cell_height + header_offset
-                right = left + cell_width
-                lower = (j+1) * cell_height
-
-                left += width_margin
-                upper += height_margin
-                right -= width_margin
-                lower -= height_margin
-
-                crop_width = right - left
-                crop_height = lower - upper
-
-                if crop_width > crop_height:
-                    right = left + crop_height
-                else:
-                    lower = upper + crop_width
-
-                code = f.readline()
-                #  print(code.strip())
-                if not code:
-                    break
-                else:
-                    name = dst_dir + "/uni" + code.strip() + ".png"
-                    #  print(name)
-                    cropped_image = img.crop((left, upper, right, lower))
-                    cropped_image = cropped_image.resize((128,128), Image.LANCZOS)
-                    cropped_image.save(name)
-        print("Processed frequency page " + str(page))
+from PIL import ImageEnhance
 
 def crop_image_uniform(src_dir, dst_dir):
     f = open("399-uniform.txt", "r")
@@ -58,13 +12,18 @@ def crop_image_uniform(src_dir, dst_dir):
         os.makedirs(dst_dir)
     for page in range(1,4):
         img = Image.open( src_dir + "/" + str(page) +"-uniform.png").convert('L')
-        img = img.point(lambda x : 0 if x < 192 else x, 'L')
+        # Remove noise
         img = img.point(lambda x : 255 if x > 192 else x, 'L')
+        # Increase constrast
+        enhancer = ImageEnhance.Contrast(img)
+        img = enhancer.enhance(1.5)
+        # Using median filter to remove noise
         img = img.filter(ImageFilter.MedianFilter)
+
         width, height = img.size
-        cell_width = width/cols
-        cell_height = height/rows
-        header_offset = height/rows * header_ratio
+        cell_width = width/float(cols)
+        cell_height = height/float(rows)
+        header_offset = height/float(rows) * header_ratio
         width_margin = cell_width * 0.10
         height_margin = cell_height * 0.10
 
@@ -75,26 +34,28 @@ def crop_image_uniform(src_dir, dst_dir):
                 right = left + cell_width
                 lower = (j+1) * cell_height
 
-                left += width_margin
-                upper += height_margin
-                right -= width_margin
-                lower -= height_margin
+                center_x = (left + right) / 2
+                center_y = (upper + lower) / 2
 
-                crop_width = right - left
-                crop_height = lower - upper
+                crop_width = right - left - 2*width_margin
+                crop_height = lower - upper - 2*height_margin
 
+                size = 0
                 if crop_width > crop_height:
-                    right = left + crop_height
+                    size = crop_height/2
                 else:
-                    lower = upper + crop_width
+                    size = crop_width/2
+
+                left = center_x - size;
+                right = center_x + size;
+                upper = center_y - size;
+                lower = center_y + size;
 
                 code = f.readline()
-                #  print(code.strip())
                 if not code:
                     break
                 else:
                     name = dst_dir + "/uni" + code.strip() + ".png"
-                    #  print(name)
                     cropped_image = img.crop((left, upper, right, lower))
                     cropped_image = cropped_image.resize((128,128), Image.LANCZOS)
                     cropped_image.save(name)
@@ -111,4 +72,3 @@ if __name__ == "__main__":
     cols = 12
     header_ratio = 16.5/(16.5+42)
     crop_image_uniform(args.src_dir, args.dst_dir)
-    crop_image_frequency(args.src_dir, args.dst_dir)
